c73da50f7b203d4d596139991ee6ebcc742696af
MurtadhaMohammed <mumu9943@gmail.com>
2023-12-21 23:00:05 +0300

    add socket io

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomeScreen from "./screens/HomeScreen/homeScreen";
 import LoginScreen from "./screens/LoginScreen/loginScreen";
 import { useAppStore } from "./store";
 import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 const ProtectedRoute = ({ comp }) => {
   const { isLogin, setIsLogin } = useAppStore();
@@ -21,12 +22,56 @@ const ProtectedRoute = ({ comp }) => {
 };
 
 const App = () => {
+  const [msg, setMsg] = useState("");
+  const [msgRec, setMsgRec] = useState("");
+  const [msgList, setMsgList] = useState([]);
+
+  var socket = io("http://localhost:3000", {
+    autoConnect: true,
+  });
+
+  useEffect(() => {
+    let onConnect = () => {
+      console.log("Connected");
+    };
+
+    let onEmit = (arg) => {
+      setMsgRec(arg);
+    };
+
+    socket.on("reciveMsg", onEmit);
+
+    socket.on("connect", onConnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("reciveMsg", onEmit);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (msgRec) setMsgList([...msgList, msgRec]);
+  }, [msgRec]);
+
+  const handleSend = () => {
+    socket.emit("message", msg);
+    setMsg("")
+  };
+
   return (
     <div>
-      <Routes>
+      <p>Message </p>
+      <ul>
+        {msgList.map((el, i) => (
+          <li key={i}>{el}</li>
+        ))}
+      </ul>
+      <input value={msg} onChange={(e) => setMsg(e.target.value)} />
+      <button onClick={handleSend}>Send</button>
+      {/* <Routes>
         <Route path="/" element={<ProtectedRoute comp={<HomeScreen />} />} />
         <Route path="/login" element={<LoginScreen />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 };
